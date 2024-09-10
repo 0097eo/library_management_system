0de34815fe6897769e8627cfb688bb61cb0eb3de
0097eo <emmanuelokello294@gmail.com>
2024-09-10 10:29:53 +0300

    add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import BooksManagement from './pages/BooksManagement';
+import Transactions from './pages/Transactions';
+import OverdueBooksList from './pages/OverdueBooksList';
+import MembersManagement from './pages/MembersManagement';
+import "./index.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Login/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/books-management' element={<BooksManagement/>}/>
+        <Route path='/transactions' element={<Transactions/>}/>
+        <Route path='/overdue-books' element={<OverdueBooksList/>}/>
+        <Route path='/members-management' element={<MembersManagement/>}/>
+      </Routes>
+    </Router>
   );
 }
 
